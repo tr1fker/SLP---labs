f7ecbb2f28489591c15ca222c969bccf38fd4a96
Artyom Letko <cfjhxhkxck@gmail.com>
2024-10-07 21:34:24 +0300

    test_tasks_lab1

---
+++ LaboratoryWork1/test.py
@@ -0,0 +1,28 @@
+#print(len(input().split()))
+
+
+
+# line = input() + ' '
+# counter = 0
+# length = len(line)
+# isNum = False
+# for i in range(length):
+#     if line[i].isdigit():
+#         isNum = True
+#     elif isNum:
+#         counter += 1
+#         isNum = False
+# print(counter)
+
+
+# list = ["fddfg", "fd", "dfddf", "dfdfdf"]
+# list.sort(key=len)
+
+slov = {
+    1: 31,
+    2: 34
+}
+for key, value in slov.items():
+    print(key, value)
+
+print(slov.keys())
