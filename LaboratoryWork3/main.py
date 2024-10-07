1633a9d121e2cd040b4c60c176eb7c783b085cfb
Artyom Letko <cfjhxhkxck@gmail.com>
2024-10-07 23:08:59 +0300

    third_task_Done

--- LaboratoryWork3/main.py
+++ LaboratoryWork3/main.py
@@ -1,10 +1,5 @@
 import sys
 
-
-# 1. Создать программный файл F1 в текстовом формате, записать в него
-# построчно данные, вводимые пользователем. Об окончании ввода данных
-# будет свидетельствовать пустая строка. Скопировать из файла F1 в файл F2
-# все строки, в которых более 2 слов. – 3 балла
 # 2. Создать текстовый файл (не программно). Построчно записать
 # фамилии сотрудников и величину их окладов (не менее 10 строк).
 # Определить, кто из сотрудников имеет оклад больше 10 тысяч, вывести
@@ -47,12 +42,12 @@ import sys
 
 
 def first_task():
-    print("""------------------------------------------------------------------------
-    Создать программный файл F1 в текстовом формате, записать в него
-построчно данные, вводимые пользователем. Об окончании ввода данных
-будет свидетельствовать пустая строка. Скопировать из файла F1 в файл F2
-все строки, в которых более 2 слов. – 3 балл
-------------------------------------------------------------------------""")
+    print("""----------------------------------------------------------------------------
+    Задача: Создать программный файл F1 в текстовом формате, записать в него
+построчно данные, вводимые пользователем. Об окончании ввода данных будет 
+свидетельствовать пустая строка. Скопировать из файла F1 в файл F2, все
+строки, в которых более 2 слов. – 3 балл
+----------------------------------------------------------------------------""")
     with open('Documents_task_1/F1.txt', 'w', encoding='utf-8') as file1:
         while True:
             line = input("Введите строку(пустая строка для завершения):")
@@ -69,6 +64,74 @@ def first_task():
         print("Данные успешно скопированы во 2-ой файл!")
 
 
+def second_task():
+    print("""-------------------------------------------------------------------
+    Создать текстовый файл (не программно). Построчно записать
+фамилии сотрудников и величину их окладов (не менее 10 строк).
+Определить, кто из сотрудников имеет оклад больше 10 тысяч, вывести
+фамилии этих сотрудников. Выполнить подсчёт средней величины дохода
+сотрудников.
+-------------------------------------------------------------------
+Пример файла:~
+Иванов 23543.12
+#Петров 13749.32– 3 балла
+-------------------------""")
+    with open("Documents_task_2/employees.txt", "w", encoding="utf-8") as file_employees:
+        number = None
+        while not number:
+            number = input_integer_min("Введите кол-во сотрудников(0 - выход):", 0)
+        if number == 0:
+            print("---------------\n"
+                  "Успешный выход!")
+        else:
+            for _employee in range(number):
+                name = input(f"Введите фамилию {_employee + 1}-сотрудника:")
+                salary = None
+                while not salary:
+                    salary = input_float(f"Введите зарплату {_employee + 1}-сотрудника:")
+                file_employees.write(name + " " + str(salary) + "\n")
+            print("----------------\n"
+                  "Успешная запись!\n"
+                  "----------------")
+    with open("Documents_task_2/employees.txt", "r", encoding="utf-8") as file_employees:
+        lines = file_employees.readlines()
+        number_employees = len(lines)
+        sum_salary = 0
+        list_has_good_employee = False
+        for line in lines:
+            name, salary = line.split()
+            salary = float(salary)
+            if salary >= 10000:
+                list_has_good_employee = True
+                break
+        if list_has_good_employee:
+            print("Сотрудники с окладом > 10000:")
+            for line in lines:
+                name, salary = line.split()
+                salary = float(salary)
+                sum_salary += salary
+                if salary > 10000:
+                    print(name)
+        else:
+            print("В файле нет сотрудников с окладом > 10000!")
+        print("Средняя зарплата сотрудников:", sum_salary / number_employees)
+
+
+
+
+def third_task():
+    pass
+def fourth_task():
+    pass
+
+def input_float(CONDITION_INPUT_INTEGER):
+    try:
+        line = float(input(CONDITION_INPUT_INTEGER))
+        return line
+    except ValueError:
+        print("------------------------------\n"
+              "Введено некорректное значение!")
+    return None
 
 def input_integer(CONDITION_INPUT_INTEGER):
     try:
@@ -115,11 +178,11 @@ def main():
         elif choice == 1:
             first_task()
         elif choice == 2:
-            pass
+            second_task()
         elif choice == 3:
-            pass
+            third_task()
         elif choice == 4:
-            pass
+            fourth_task()
         else:
             print("Повторите попытку!")
 
